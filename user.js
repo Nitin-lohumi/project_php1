d40dc5d7e243ff158d81848e38a99bdaa755c958
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-07-04 11:42:26 +0530

    aasS

--- user.js
+++ user.js
@@ -1,22 +1,4 @@
 let user_list = document.getElementById("userlist");
-
-// let searchbar = document.getElementById("searchbar");
-// let searchbtn = document.getElementById("searchbtn");
-// searchbar.onkeyup = () =>{
-//   let searchTerm = searchbar.value;
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("POST","php/search.php",true);
-//   xhr.onload =()=>{
-//        if(xhr.readyState==XMLHttpRequest.DONE){
-//           if(xhr.status==200){
-//               let data = xhr.response;
-//                console.log(data);
-//           }
-//        }
-//   }
-//   xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-//   xhr.send("searchTerm="+searchTerm);
-// }
 setInterval(()=>{
     let xhr = new XMLHttpRequest();
     xhr.open("GET","php/users.php",true);
